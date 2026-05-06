ba8c30c5a1532471168f8d78145982cf45343079
Iceapplec <dhpinkanny0222@gmail.com>
2026-05-06 12:59:29 +0900

    리더보드 패널

--- MidTestProject_26/Assets/Scripts/PanelManager.cs
+++ MidTestProject_26/Assets/Scripts/PanelManager.cs
@@ -4,12 +4,19 @@ public class PanelManager : MonoBehaviour
 {
     // Inspector에 LeaderPanel GameObject 할당 (비활성 상태여도 OK)
     public GameObject leaderPanel;
+    // STAGE 버튼에 연결할 HighScore 패널들
+    public GameObject highScore1Panel;
+    public GameObject highScore2Panel;
 
-    private void Awake()
+    private void Awake()    
     {
         // 안전하게 초기에는 숨김(Inspector에서 이미 비활성화해도 상관 없음)
         if (leaderPanel != null)
             leaderPanel.SetActive(false);
+        if (highScore1Panel != null)
+            highScore1Panel.SetActive(false);
+        if (highScore2Panel != null)
+            highScore2Panel.SetActive(false);
     }
 
     // 버튼에서 호출: 패널을 켬
@@ -31,5 +38,40 @@ public class PanelManager : MonoBehaviour
     {
         if (leaderPanel != null)
             leaderPanel.SetActive(!leaderPanel.activeSelf);
+    }
+
+    // STAGE1 버튼에서 호출: HighScore1 패널을 엶
+    public void ShowHighScore1()
+    {
+        if (highScore1Panel != null)
+            highScore1Panel.SetActive(true);
+    }
+
+    // STAGE2 버튼에서 호출: HighScore2 패널을 엶
+    public void ShowHighScore2()
+    {
+        if (highScore2Panel != null)
+            highScore2Panel.SetActive(true);
+    }
+
+    // 공용: 모든 HighScore 패널 닫기
+    public void HideHighScores()
+    {
+        if (highScore1Panel != null)
+            highScore1Panel.SetActive(false);
+        if (highScore2Panel != null)
+            highScore2Panel.SetActive(false);
+    }
+
+    public void HideSTAGE1()
+    {
+        if (highScore1Panel != null)
+            highScore1Panel.SetActive(false);
+    }
+
+    public void HideSTAGE2()
+    {
+        if (highScore2Panel != null)
+            highScore2Panel.SetActive(false);
     }
 }
